9cf7d87d266a2f60b87ccef2c83e724065fc88bf
Juan Sebastian Arboleda <sebastipolo@gmail.com>
2020-10-12 13:22:40 +0200

    added homepage and heroku integration

--- StatisticsGradingApp/settings.py
+++ StatisticsGradingApp/settings.py
@@ -19,13 +19,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '8p_baoklml$y6l+onwjd8@6tg)meo=7k(33nxc%^@0+r(ih*a+'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+# SECURITY WARNING: keep the secret key used in production secret!
+SECRET_KEY = '8p_baoklml$y6l+onwjd8@6tg)meo=7k(33nxc%^@0+r(ih*a+'
+
+ALLOWED_HOSTS = ['statistics-grading-app.herokuapp.com', '127.0.0.1']
 
 
 # Application definition
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'home',
 ]
 
 MIDDLEWARE = [
@@ -118,3 +119,4 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
